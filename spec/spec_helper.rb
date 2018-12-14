9ebd35ac719f5f71a3a7995c8843b980be0d5f5d
Matthias Guenther <matthias@wikimatze.de>
2018-12-14 22:35:05 +0100

    No coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,3 @@
-require 'coveralls'
-Coveralls.wear!
-
-
 RACK_ENV = 'test' unless defined?(RACK_ENV)
 require File.expand_path(File.dirname(__FILE__) + '/../config/boot')
 require File.dirname(__FILE__) + '/factories'
